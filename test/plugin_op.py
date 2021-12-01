8c29796151d0379c47743f68ec2283632550997a
Ciara Daly <72494811+ciaradaly@users.noreply.github.com>
2021-12-01 12:23:51 +0000

    tbi: display basic information about current scene

--- test/plugin_op.py
+++ test/plugin_op.py
@@ -4,7 +4,7 @@ from bpy.types import Operator
 
 class CD_OT_Apply_All_Op(Operator):
     bl_idname = "object.apply_all_mods"
-    bl_label = "Apply all"
+    bl_label = "bpy.data.scenes.keys()"
     bl_description = "Apply all operators of the active object"
 
     @classmethod
@@ -30,7 +30,7 @@ class CD_OT_Apply_All_Op(Operator):
 
 class CD_OT_Cancel_All_Op(Operator):
     bl_idname = "object.cancel_all_mods"
-    bl_label = "Cancel all"
+    bl_label = "bpy.data.scenes.keys()"
     bl_description = "Cancel all operators of the active object"
 
     @classmethod
--- test/plugin_pnl.py
+++ test/plugin_pnl.py
@@ -6,16 +6,19 @@ class CD_PT_PANEL(Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_label = "Modifier operations"
-    bl_category = "JModifier Util"
+    bl_category = "Display Info"
 
     def draw(self, context):
 
         layout = self.layout
 
-        # 2 columns with button
-        row = layout.row()
-        col = row.column()
-        col.operator("object.apply_all_mods", text = "Apply all")
+        layout = self.layout
 
-        col = row.column()
-        col.operator("object.cancel_all_mods", text = "Cancel all")
+        box = layout.box()
+
+        for obj in bpy.data.objects:
+            print(obj.name)
+            # 2 columns with button
+            row = layout.row()
+            col = row.column()
+            col.label(text = obj.name)
