75dddb93e2a7d5b0b28e52d3eca731c01c643344
Ciara Daly <72494811+ciaradaly@users.noreply.github.com>
2022-02-01 15:09:16 +0000

    update: UI buttons now change location and viewpoint

--- test/plugin_pnl.py
+++ test/plugin_pnl.py
@@ -2,23 +2,31 @@ import bpy
 
 from bpy.types import Panel
 
-class CD_PT_PANEL(Panel):
+from . plugin_op import ButtonOperator
+
+class OBJECT_PT_MyPanel(Panel):
+    bl_idname = "object.my_panel"
+    bl_label = "Location Operator"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
-    bl_label = "Modifier operations"
-    bl_category = "Display Info"
+    bl_category = "Blender Plugin"
+    bl_context = "objectmode"
 
+    @classmethod
+    def poll(self,context):
+        return context.object is not None
+
+    
     def draw(self, context):
-
-        layout = self.layout
-
-        layout = self.layout
-
-        box = layout.box()
-
-        for obj in bpy.data.objects:
-            print(obj.name)
-            # 2 columns with button
-            row = layout.row()
-            col = row.column()
-            col.label(text = obj.name)
+        layout = self.layout # .operator("scene.button_operator")
+        layout.operator('object.button_operator', text="Change Location", icon="CONSOLE").action='LOCATION'
+        layout.operator('object.button_operator', text="Change ViewPoint", icon="CONSOLE").action='CHANGE_VIEW'
+        # obj = bpy.context.selected_objects
+        # row = layout.row()
+        # col = row.column(align=True)
+        # layout.label(text="Location adjustment:")
+        # # col.label(text = str(obj.location))
+        # col = layout.column(align=True)
+        # col.operator(ButtonOperator.bl_idname, text="Change Location", icon="CONSOLE")
+        
+        # col.operator(ChangeViewpointOperator.bl_idname, text="Change ViewPoint", icon="CONSOLE")
