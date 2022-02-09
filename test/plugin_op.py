1fde2edc988d1dbd1c4dc9a0178af51deb33ca49
Ciara Daly <72494811+ciaradaly@users.noreply.github.com>
2022-02-09 13:47:41 +0000

    update: bone creation from bezier now a functional ui component

--- test/plugin_op.py
+++ test/plugin_op.py
@@ -10,7 +10,8 @@ class ButtonOperator(Operator):
     action: EnumProperty(
         items=[
             ('LOCATION', 'change location', 'change location'),
-            ('CHANGE_VIEW', 'change viewport', 'change viewport')
+            ('CHANGE_VIEW', 'change viewport', 'change viewport'),
+            ('CONVERT_BONES', 'convert to armature', 'convert to armature')
         ]
     )
     def execute(self, context):
@@ -18,25 +19,56 @@ class ButtonOperator(Operator):
             self.change_location(context=context)
         if self.action == 'CHANGE_VIEW':
             self.change_viewport(context=context)
+        if self.action == 'CONVERT_BONES':
+            self.bone_creator(context=context)
         return {'FINISHED'}
 
     @staticmethod
     def change_location(context):
         bpy.context.active_object.location = (0, 0, 0)
         return {'FINISHED'}
-
-# class ChangeViewpointOperator(Operator):
-#     bl_idname = "object.button_operator"
-#     bl_label = "Test"
+        
     @staticmethod
     def change_viewport(context):
         for area in bpy.context.screen.areas:
             if area.type == 'VIEW_3D': 
+                #TBI: look from z, x and y axis by pressing button
+                #check https://docs.blender.org/api/current/bpy.ops.view3d.html
                 ctx = {
-                    "window": bpy.context.window, # Only change on window currently open
-                    "area": area, # our 3D View (the first found only actually)
-                    "region": None # just to suppress PyContext warning, doesn't seem to have any effect
+                    "window": bpy.context.window, 
+                    "area": area, # Only change on window currently open
                 }
-                bpy.ops.view3d.view_axis(type='LEFT', align_active=False)
+                bpy.ops.view3d.view_axis(ctx, type='RIGHT', align_active=False)
                 bpy.context.space_data.region_3d.update()
         return {'FINISHED'}
+
+    @staticmethod
+    def bone_creator(context):
+        # context = bpy.context
+        # arm_base = context.object
+        # bone_base = arm_base.data
+        obj = bpy.context.view_layer.objects.active
+        #checks to see if selected object is a bezier curve
+        if (obj.type == "CURVE"):
+            #convert to mesh
+            bpy.ops.object.convert(target='MESH', keep_original=True)
+            #add skin modifier armature to mesh
+            obj.hide_set(True)
+            obj = bpy.context.view_layer.objects.active
+            bpy.ops.object.modifier_add(type='SKIN')
+            bpy.ops.object.skin_armature_create(modifier='Skin')
+            #hide bezier curve in mesh state after creation
+            obj.hide_set(True)
+            # show octagonal shape of bones
+            created_bones = bpy.context.view_layer.objects.active
+            bpy.data.armatures[created_bones.name].display_type = 'OCTAHEDRAL'
+            # bpy.ops.object.mode_set(mode='EDIT')
+            # bpy.ops.armature.select_all(action='SELECT')
+            # bpy.ops.armature.dissolve()
+            bpy.context.space_data.region_3d.update()
+
+            #https://www.youtube.com/watch?v=mGsRmAq9mNU&ab_channel=StevenScott
+
+        return {'FINISHED'}
+
+
--- test/plugin_pnl.py
+++ test/plugin_pnl.py
@@ -4,14 +4,17 @@ from bpy.types import Panel
 
 from . plugin_op import ButtonOperator
 
+
 class OBJECT_PT_MyPanel(Panel):
     bl_idname = "object.my_panel"
-    bl_label = "Location Operator"
+    bl_label = "Armature Assistant"
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_category = "Blender Plugin"
     bl_context = "objectmode"
 
+    bpy.types.Scene.MyInt = bpy.props.IntProperty(name='MyInt', description="Amount of Bones", min=0, max=100, default=1)
+    
     @classmethod
     def poll(self,context):
         return context.object is not None
@@ -19,8 +22,11 @@ class OBJECT_PT_MyPanel(Panel):
     
     def draw(self, context):
         layout = self.layout # .operator("scene.button_operator")
-        layout.operator('object.button_operator', text="Change Location", icon="CONSOLE").action='LOCATION'
+        layout.operator('object.button_operator', text="Move to Center", icon="CONSOLE").action='LOCATION'
         layout.operator('object.button_operator', text="Change ViewPoint", icon="CONSOLE").action='CHANGE_VIEW'
+        layout.operator('object.button_operator', text="Bone converter", icon="CONSOLE").action='CONVERT_BONES'
+
+        layout.prop(bpy.context.scene, "MyInt", slider=True)
         # obj = bpy.context.selected_objects
         # row = layout.row()
         # col = row.column(align=True)
